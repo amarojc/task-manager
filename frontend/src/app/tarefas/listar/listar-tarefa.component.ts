b9e16415756f6caeeba633c3e0465cdcaa4fb9a8
Jorge Amaro <inf.amaro.jc@gmail.com>
2024-08-13 16:59:57 -0300

    Task list Component created

--- frontend/src/app/tarefas/listar/listar-tarefa.component.ts
+++ frontend/src/app/tarefas/listar/listar-tarefa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TarefaService, Tarefa } from '../shared';
 
 @Component({
   selector: 'app-listar-tarefa',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarTarefaComponent implements OnInit {
 
-  constructor() { }
+  tarefas: Tarefa[];
+  
+  constructor(private tarefaService: TarefaService) { }
 
   ngOnInit(): void {
+    this.tarefas = this.findAll();
   }
+
+  findAll(): Tarefa[]{
+    return this.tarefaService.findAll();
+  }
+
+
+  /*
+  delete($event: any, tarefa: Tarefa): void{
+    $event.preventDefault();
+    if(confirm('Deseja remover a tarefa "' + tarefa.nome + '"?')){
+      this.tarefaService.delete(tarefa.id);
+      this.tarefas = this.tarefaService.findAll();
+    }
+  }
+  */
 
 }
